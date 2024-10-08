cf0d1b18d017ac04557880b59e37470b776e470a
thwahir <thwahirpvmohd@gmail.com>
2024-10-08 08:50:20 +0530

    adding programme of quick sorting with taking median as pivot

--- Sorting/Quick_sort/quick_sort.py
+++ Sorting/Quick_sort/quick_sort.py
@@ -1,4 +1,4 @@
-import random
+import random, statistics
 
 class Quick_sort:
     def pivot_place_first(self, arr, first, last):
@@ -54,10 +54,41 @@ class Quick_sort:
         arr[last], arr[left] = arr[left], arr[last]
         return left
 
+    def pivot_place_median(self, arr, first, last):
+        low = arr[first]
+        mid = (first+last)//2
+        heigh = arr[last]
+        pivot = statistics.median([low, arr[mid], heigh])
+        p_index = len(arr) - 1
+
+        if pivot == low:
+            p_index = first
+        elif pivot == arr[mid]:
+            p_index = mid
+        else:
+            p_index = last
+
+        arr[p_index], arr[last] = arr[last], arr[p_index]
+        pivot = arr[last]
+        left = first 
+        right = last - 1
+        
+        while True:
+            while left<=right and arr[left] <= pivot:
+                left = left + 1
+            while left<=right and arr[right] >= pivot:
+                right = right - 1
+            
+            if left > right:
+                break
+            else:
+                arr[left], arr[right] = arr[right], arr[left]
+        arr[last], arr[left] = arr[left], arr[last]
+        return left        
 
     def quick_sort(self, arr, first, last):
         if first<last:
-            p_index = self.pivot_place_random(arr, first, last)
+            p_index = self.pivot_place_median(arr, first, last)
             self.quick_sort(arr, first, p_index-1)
             self.quick_sort(arr, p_index+1, last)
         return arr
